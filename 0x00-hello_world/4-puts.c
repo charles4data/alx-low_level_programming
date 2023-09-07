efcc7f11fe2bdcd415be677887679a93b7e985c1
Julius Charles <cjmatheka@gmail.com>
2023-09-07 11:36:22 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * entry point -main
- * function prints a string
- * function returns zero
- * Using printf now allowed
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
