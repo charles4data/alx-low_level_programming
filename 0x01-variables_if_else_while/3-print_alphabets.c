48c3703f9c21cabc16d23e5bbbbb08d2703379ee
Julius Charles <cjmatheka@gmail.com>
2023-09-09 01:32:16 -0700

    update 3-print_alphabet.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,13 +11,18 @@ int main(void)
 	char ch_lower = 'a';
 	char ch_upper = 'A';
 
-	while (ch_lower <= 'z' && ch_upper <= 'Z')
+	while (ch_lower <= 'z')
 	{
 		putchar(ch_lower);
-		putchar(ch_upper);
 		ch_lower++;
+	}
+
+	while (ch_upper <= 'Z')
+	{
+		putchar(ch_upper);
 		ch_upper++;
 	}
+
 	putchar('\n');
 
 	return (0);
