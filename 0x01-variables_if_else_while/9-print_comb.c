ea99c67343c766c18a5ad5221c6480adc997e523
Julius Charles <cjmatheka@gmail.com>
2023-09-11 06:32:08 -0700

    update 9-print_comb.c program

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,12 @@
 int main(void)
 {
 	int num1;
-	int num2;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		putchar(num1 + '0');
 		putchar(',');
 		putchar(' ');
-
-		for (num2 = 0; num2 <= 9; num2++)
-		{
-			putchar(num2 + '0');
-		}
 	}
 	return (0);
 }
