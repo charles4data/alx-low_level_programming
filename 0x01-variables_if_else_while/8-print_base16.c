265e6ed1d757c3eac33349069f54cb58f3122091
Julius Charles <cjmatheka@gmail.com>
2023-09-11 06:03:10 -0700

    Add 8-print_base16.c program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main function starts here
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch10;
+	char af6;
+
+	for (ch10 = '0'; ch10 <= 9; ch10++)
+	{
+		putchar(ch10);
+	}
+
+	for (af6 = 'a'; af6 <= 'f'; af6++)
+	{
+		putchar(af6);
+	}
+
+	putchar('\n');
+	return (0);
+}
