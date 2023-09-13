b791fb37c0a89b08f5a20c1b5fbbe3ac678b7709
Julius Charles <cjmatheka@gmail.com>
2023-09-13 12:32:27 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,17 +19,17 @@ void times_table(void)
 		{
 			ij = i * j;
 
-			_putchar(ij);
+			putchar(ij + '0');
 
 			if (j < 9)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
 
 			else
 			{
-				_putchar('\n');
+				putchar('\n');
 			}
 		}
 	}
