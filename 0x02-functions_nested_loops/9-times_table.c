50e5c685119cf31477379eb0688502afa9309cf8
Julius Charles <cjmatheka@gmail.com>
2023-09-13 12:28:32 -0700

    Add 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,22 +11,25 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int ij;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			putchar(i * j + '0');
+			ij = i * j;
+
+			_putchar(ij + '0');
 
 			if (j < 9)
 			{
-				putchar(',');
-				putchar(' ');
+				_putchar(',');
+				_putchar(' ');
 			}
+
 			else
 			{
-				putchar('$');
-				putchar('\n');
+				_putchar('\n');
 			}
 		}
 	}
