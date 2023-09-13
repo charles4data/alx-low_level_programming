86c48e4a326d3936bbe1fcfafecf5c01fa4915a2
Julius Charles <cjmatheka@gmail.com>
2023-09-13 14:06:24 -0700

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,16 +9,34 @@
 
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n <= 98)
 	{
-		printf("%d", n);
-
-		if (n < 98)
+		while (n <= 98)
 		{
-			putchar(',');
-			putchar(' ');
+			printf("%d", n);
+
+			if (n < 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n++;
 		}
-		n++;
+	}
+
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+
+			if (n > 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n--;
+		}
 	}
 	putchar('\n');
 }
