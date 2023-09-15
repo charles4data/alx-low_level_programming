1bb817c49be64dafc26ce6c25e2b8fed64a6bca5
Julius Charles <cjmatheka@gmail.com>
2023-09-15 12:41:46 -0700

    Add 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: Parameter
+ * return: Always 0.
+ */
+
+void print_line(int n)
+{
+	for (n = 0; n <= 30; n++)
+	{
+		_putchar(_ + '0');
+	}
+	_putchar('\n');
+}
