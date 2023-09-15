496ec208730b3e81a62107fd1bd2825094cc7fc3
Julius Charles <cjmatheka@gmail.com>
2023-09-15 13:14:58 -0700

    Update 6-print_line.c 1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,9 +9,18 @@
 
 void print_line(int n)
 {
-	for (n = 0; n <= 30; n++)
+	int i;
+
+	if (n <= 0)
 	{
-		_putchar(_ + '0');
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 }
