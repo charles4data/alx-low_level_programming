2c1e654b4d53e1f0eb9c151d9dfaa8c40730a272
Julius Charles <cjmatheka@gmail.com>
2023-09-15 17:19:53 -0700

    Update 5-more_numbers.c Final

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,21 +12,16 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
 			{
-				_putchar('1');
-				_putchar(j % 10 + '0');
+				putchar(j / 10 + '0');
 			}
-			else
-			{
-				_putchar(j + '0');
-			}
+			putchar(j % 10 + '0');
 		}
-		printf("\n");
+		putchar('\n');
 	}
-	printf("\n");
 }
