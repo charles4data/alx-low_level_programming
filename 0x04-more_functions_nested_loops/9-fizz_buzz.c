69f51e43f191a88995cce331ffb6fd7d3021feff
Julius Charles <cjmatheka@gmail.com>
2023-09-15 17:44:46 -0700

    Debug 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,31 +12,33 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0)
 		{
-			printf("Fizzbuzz");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz");
+			printf(" Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz");
+			printf(" Buzz");
+		}
+
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
 		}
 		else
 		{
-			printf("%d", i);
+			if (i == 1)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf(" %d", i);
+			}
 		}
-		if (i == 100)
-		{
-			printf("\n");
-		}
-		else
-		{
-			printf(" ");
-		}
+
 	}
+	printf("\n");
 
 	return (0);
 }
