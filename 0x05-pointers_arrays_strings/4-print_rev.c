9dce7db4d6d19b37b19e6348b8abe1c88ba7c288
Julius Charles <cjmatheka@gmail.com>
2023-09-18 14:09:11 -0700

    Add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * print_rev - prints a strong in reverse.
+ * @s: string to be printed
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
