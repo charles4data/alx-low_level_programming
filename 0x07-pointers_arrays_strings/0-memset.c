0a257fdfaa92ae376f50e5c97eff391611f4dd7b
Julius Charles <cjmatheka@gmail.com>
2023-09-26 11:05:49 -0700

    Add 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _memset - fills memory with a constant byte
+ * @n: parameter
+ * @s: parameter
+ * @b: parameter
+ * Return: Always s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *p = s;
+
+	while (n-- > 0)
+	{
+		*p++ = b;
+	}
+	return (s);
+}
