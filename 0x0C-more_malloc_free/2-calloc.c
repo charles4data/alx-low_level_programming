b9006ea79e8840c2542aa872a60415e33003e673
Julius Charles <cjmatheka@gmail.com>
2023-10-06 15:18:51 -0700

    Update 2-calloc.c again

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * _calloc - allocates memory for an array,
@@ -11,7 +12,9 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p = malloc(sizeof(int) * nmemb * size);
+	void *p = calloc(nmemb, nmemb * size);
+
+	memset(p, 0, nmemb * size);
 
 	if (nmemb == 0 || size == 0)
 	{
