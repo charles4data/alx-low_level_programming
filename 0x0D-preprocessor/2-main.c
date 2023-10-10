4182c70ce13cfa0f7c751baf3b3ec4e9a164a294
Julius Charles <cjmatheka@gmail.com>
2023-10-10 15:23:51 -0700

    Add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - prints the source file name
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
