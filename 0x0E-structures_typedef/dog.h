e8c42a8a8bee712d0cbd7f877311523b2a37254d
Julius Charles <cjmatheka@gmail.com>
2023-10-10 19:43:46 -0700

    Update fruther dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,13 @@ typedef struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * struct dog1 - a new dog
+ * @name: dog name,
+ * @age: dog age,
+ * @owner: dog owner.
+ */
+
 typedef struct dog1
 {
 	char *name;
