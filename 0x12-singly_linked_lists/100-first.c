ecb6774d3228b00208c76fcf3409ff56f598dbd3
Julius Charles <cjmatheka@gmail.com>
2023-10-19 20:02:13 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,7 +9,7 @@
 
 void __attribute__ ((constructor)) print_before_main();
 
-void print_before_main()
+void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
