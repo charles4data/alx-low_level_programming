5319f11fb83cb7d95400bb6076d690312bef4c9f
Julius Charles <cjmatheka@gmail.com>
2023-10-19 19:59:12 -0700

    Add 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+/*
+ * __attribute__ - prints before main
+ * @constructor: not much,
+ * Return: Nothing.
+ */
+
+void __attribute__ ((constructor)) print_before_main();
+
+void print_before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
