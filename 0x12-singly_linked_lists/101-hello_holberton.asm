e4aafd9dad67cb30de285de436db19da9f2f2767
Julius Charles <cjmatheka@gmail.com>
2023-10-20 09:01:26 -0700

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,31 +1,32 @@
-; declare the printf function prototype
-extern printf
+; Define some constants
+    SECTION .data
+msg:    db "Hello, Holberton", 10, 0 ; The string to print, 10 is newline
+len:    equ $-msg                   ; The length of the string
+fmt:    db "%s", 0                  ; The format string for printf
 
-; declare the .data section
-section .data
-    ; define the message to print
-    msg db "Hello, Holberton", 10, 0 ; 10 is the newline character, 0 is the null terminator
+; Declare external functions
+    extern printf
 
-; declare the .text section
-section .text
-    ; define the main function
+; Define the code section
+    SECTION .text
     global main
 
 main:
-    ; save the stack pointer in rbp
+; Save the base pointer and stack pointer
     push rbp
     mov rbp, rsp
 
-    ; set the first argument for printf (rcx) to the address of the message
-    mov rcx, msg
+; Pass the arguments to printf: fmt and msg
+    mov rdi, fmt     ; First argument is the format string
+    mov rsi, msg     ; Second argument is the message string
+    xor rax, rax     ; Clear rax to indicate no floating point arguments
+    call printf      ; Call the printf function
 
-    ; call the printf function
-    call printf
-
-    ; restore the stack pointer from rbp
+; Restore the base pointer and stack pointer
     mov rsp, rbp
     pop rbp
 
-    ; exit with code 0
-    mov eax, 0
+; Return 0 as exit code
+    mov rax, 0
     ret
+
