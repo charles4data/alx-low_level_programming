25f7673bc9b8e9158a952891e985c71599764ee3
Julius Charles <cjmatheka@gmail.com>
2023-10-25 07:01:50 -0700

    Add 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string,
+ * @s: parameter to print,
+ * Return: Nothing.
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
