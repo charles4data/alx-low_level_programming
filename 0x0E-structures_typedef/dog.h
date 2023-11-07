5ffa2a6ef8b4604d6634a2db7900203424b4060c
Julius Charles <cjmatheka@gmail.com>
2023-11-07 15:34:04 -0800

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - data structure defined,
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
