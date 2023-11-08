e6556e1f877127b4ade92bf522cd4dbdd72d5b66
Julius Charles <cjmatheka@gmail.com>
2023-11-07 16:47:38 -0800

    Add 5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs,
+ * @d: dog to free,
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((*d).name);
+		free((*d).owner);
+	}
+	free(d);
+}
+
