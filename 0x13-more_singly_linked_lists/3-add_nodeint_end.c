ed7bef0210580483de7b943437e45aa7dc1f305f
Julius Charles <cjmatheka@gmail.com>
2023-11-22 17:35:50 -0800

    Add 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,31 +8,35 @@
  * @n: the integer to be stored in the new node
  * Return: the address of the new element, or NULL if it failed
  */
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new_node;
-    listint_t *last_node;
+	listint_t *new_node;
+	listint_t *last_node;
 
-    new_node = malloc(sizeof(listint_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(listint_t));
 
-    new_node->n = n;
-    new_node->next = NULL;
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 
-    if (*head == NULL)
-    {
-        *head = new_node;
-    }
-    else
-    {
-        last_node = *head;
-        while (last_node->next != NULL)
-        {
-            last_node = last_node->next;
-        }
-        last_node->next = new_node;
-    }
+	new_node->n = n;
+	new_node->next = NULL;
 
-    return (new_node);
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		last_node = *head;
+
+		while (last_node->next != NULL)
+		{
+			last_node = last_node->next;
+		}
+		last_node->next = new_node;
+	}
+	return (new_node);
 }
