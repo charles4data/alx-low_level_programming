8977450b4b30b77ab67e97cb5bf4c3fa29897fe7
Julius Charles <cjmatheka@gmail.com>
2023-11-24 15:58:18 -0800

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,23 +11,28 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result;
-	char current;
+	int i;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
 
-	while ((current = *b++) != '\0')
+	result = 0;
+	i = 0;
+
+	while (b[i] != '\0')
 	{
-		if (current == '0' || current == '1')
+		if (b[i] == '0' || b[i] == '1')
 		{
-			result = (result << 1) + (current - '0');
+			result = result << 1;
+			result = result + (b[i] - '0');
 		}
 		else
 		{
 			return (0);
 		}
+		i++;
 	}
 	return (result);
 }
