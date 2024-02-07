354ec6b8e6ae9f344f72d4c86788d45039fae9a6
cjmatheka <cjmatheka@gmail.com>
2024-02-08 00:17:12 +0300

    Update Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,38 +9,51 @@
  */
 
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
+int hash_table_set(hash_table_t *ht, const char *key, const char *value) {
 	hash_node_t *new_node, *temp;
+	unsigned long int index;
 
-	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
-	{
+	new_node = NULL;
+	temp = NULL;
+
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
-	}
 
-	index = hash_djb2((const unsigned char *)key) % ht->size;
+	index = key_index((unsigned char *) key, ht->size);
+	temp = ht->array[index];
 
 	/* Check if the key already exists in the linked list */
-	for (temp = ht->array[index]; temp != NULL; temp = temp->next)
-	{
-		if (strcmp(temp->key, key) == 0)
-		{
+	while (temp) {
+		if (strcmp(temp->key, key) == 0) {
+			/* Free the old value */
 			free(temp->value);
+
+			/* Duplicate the new value */
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
 			return (1);
 		}
+		temp = temp->next;
 	}
 
-	/* Key not found, create a new node and add it to the beginning of the linked list */
+	/* Key not found, create a new node and add it to the beginning of the list */
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-	{
+		return (0);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL) {
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL) {
+		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
