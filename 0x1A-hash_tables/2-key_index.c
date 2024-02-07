5a542bebfe82baa6fec8d1499ce03b106ce5be1e
cjmatheka <cjmatheka@gmail.com>
2024-02-07 22:40:28 +0300

    Add Task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * ke_index - returns the index of as key
+ * @key: the key whose index is needed
+ * @size: size of the array
+ * Return: index of provided key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hashValue;
+
+	hashValue = hash_djb2(key);
+
+	return (hashValue % size);
+}
