e79eeec2e117a45bdf56f431be2092554245a87e
cjmatheka <cjmatheka@gmail.com>
2024-05-07 23:06:21 +0300

    Add Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+  * linear_search - searches for item in a list
+  *@array: array to be searched
+  *@size: size of the array
+  *@value: value to search for
+  * Return: Index of the searched value
+  */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return(-1);
+	}
+
+	for (i =0; i < (int)size; i++)
+	{
+		printf("Value checked array[%u] = [%d]\n", i, array[i]);
+		if (value == array[i])
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/* include c libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+/* Function Prototypes*/
+int linear_search(int *array, size_t size, int value);
+
+#endif 
